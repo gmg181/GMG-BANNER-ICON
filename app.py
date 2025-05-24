ecac297ba835e5bb60ffd40d23e4e30f846e7c50
gmg181 <mapa80657@gmail.com>
2025-05-24 10:52:55 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, send_from_directory, jsonify
+import os
+
+app = Flask(__name__)
+IMAGE_FOLDER = os.path.join(os.getcwd(), 'images')
+
+@app.route('/api/icon/<icon_id>', methods=['GET'])
+def get_icon(icon_id):
+    filename = f"{icon_id}.png"
+    filepath = os.path.join(, filename)
+    
+    if os.path.exists(filepath):
+        return send_from_directory(IMAGE_FOLDER, filename)
+    else:
+        return jsonify({'error': 'Icon not found'}), 404
+
+if __name__ == '__main__':
+    print(f"[INFO] Image folder: {IMAGE_FOLDER}")
+    app.run(debug=True)
