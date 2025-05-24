0f3dd115704e0e21ab6aced30f45213e554aba56
gmg181 <mapa80657@gmail.com>
2025-05-24 10:53:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, send_from_directory, jsonify
 import os
 
 app = Flask(__name__)
-IMAGE_FOLDER = os.path.join(os.getcwd(), 'images')
+IMAGE_FOLDER = os.path.join(os.getcwd(), 'outfit')
 
 @app.route('/api/icon/<icon_id>', methods=['GET'])
 def get_icon(icon_id):
     filename = f"{icon_id}.png"
-    filepath = os.path.join(, filename)
+    filepath = os.path.join(IMAGE_FOLDER, filename)
     
     if os.path.exists(filepath):
         return send_from_directory(IMAGE_FOLDER, filename)
